23837e67d5ebe707cf98c0b259c76097b8b60f7a
Cody VanGosen <64549336+codyvangosen@users.noreply.github.com>
2025-11-25 09:21:57 -0600

    Add files via upload
    Insert rows into the Boxoffice and Movies tables.

---
+++ lesson_13_inserting_rows/insert_rows.sql
@@ -0,0 +1,7 @@
+INSERT INTO movies
+(Id, Title, Director, Year, Length_minutes)
+VALUES (15, "Toy Story 4", "Lee Unkrich", 2015, 106);
+
+INSERT INTO boxoffice
+(Movie_id, Rating, Domestic_sales, International_sales)
+VALUES (15, 8.7, 340000000, 270000000)
